445a71eae534e1a4da1d9f9056191969ee90f0d9
themoon41 <83379725+themoon41@users.noreply.github.com>
2021-06-07 00:30:25 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ async def on_member_join(member):
 
 
 
-#Bot commands
+#Basic bot commands
 
 @bot.command(help='say hi.')
 async def hello(ctx):
@@ -52,6 +52,76 @@ async def hello(ctx):
 async def quote(ctx):
   responds = get_quote()
   await ctx.send(responds)
+
+  
+  # Level Up system
+
+@bot.event
+async def on_member_json(member):
+  with open('users.json', 'r') as f:
+    users = json.load(f)
+  
+  
+  await update_data(users, member)
+
+  with open('users.json', 'w') as f:
+    json.dump(users, f)
+
+
+@bot.event
+async def on_message(message):
+  if message.authpr.bot == False:
+    with open('wsuers.json', 'r') as f:
+      users = json.load(f)
+
+    await update_data(users, message.author)
+    await add_experience(users, message.author, 5)
+    await level_up(users, message.author, message)
+
+    with open('users.json', 'w') as f:
+      json.dump(users, f)
+    
+    await bot.process_commands(message)
+
+
+
+async def update_data(users, user):
+  if not f'{user.id}' in users:
+    users[f'{user.id}'] = {}
+    users[f'{user.id}']['experience'] = 0
+    users[f'{user.id}']['level'] = 1
+
+
+async def add_experience(users, user, exp):
+  users[f'{user.id}']['experience'] += exp
+
+
+async def level_up(users, user, message):
+    with open('levels.json', 'r') as g:
+        levels = json.load(g)
+    experience = users[f'{user.id}']['experience']
+    lvl_start = users[f'{user.id}']['level']
+    lvl_end = int(experience ** (1 / 4))
+    if lvl_start < lvl_end:
+        await message.channel.send(f'{user.mention} has leveled up to level {lvl_end}')
+        users[f'{user.id}']['level'] = lvl_end
+    
+  
+@bot.command(help= 'shows current level')
+async def level(ctx, member: discord.Member = None):
+    if not member:
+        id = ctx.message.author.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'You are at level {lvl}!')
+    else:
+        id = member.id
+        with open('users.json', 'r') as f:
+            users = json.load(f)
+        lvl = users[str(id)]['level']
+        await ctx.send(f'{member} is at level {lvl}!')
+
 
 
 # Tic Tac Toe
